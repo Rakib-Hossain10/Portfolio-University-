34c4b389ec90f80fe19084b46f52d4feb3ab4563
Rakib-Hossain10 <iamrakib1221@gmail.com>
2026-04-21 21:20:09 +0800

    fixed bugs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,6 +44,19 @@ const About = () => {
           </p>
           {/* Resume Button */}
 
+          <a
+            href="https://drive.google.com/file/d/1aWc5M3Hz0DmuGyXRKvsmncMlFehl7EF3/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD RESUME
+          </a>
+
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
